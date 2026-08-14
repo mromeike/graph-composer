5998eb9a46e61734d369972355cf679de6cb7b7f
Marcel Romeike <marcel.romeike@asambeauty.com>
2026-08-14 15:07:34 +0200

    Migrate to `humbug/box` for PHAR compilation

--- bin/build.php
+++ bin/build.php
@@ -1,28 +1,26 @@
 <?php
 
 // explicitly give VERSION via ENV or ask git for current version
-$version = getenv('VERSION');
+$version = \getenv('VERSION');
 if ($version === false) {
-    $version = ltrim(exec('git describe --always --dirty', $_, $code), 'v');
+    $version = \ltrim(\exec('git describe --always --dirty', result_code: $code), 'v');
+
     if ($code !== 0) {
-        fwrite(STDERR, 'Error: Unable to get version info from git. Try passing VERSION via ENV' . PHP_EOL);
+        \fwrite(\STDERR, 'Error: Unable to get version info from git. Try passing VERSION via ENV' . \PHP_EOL);
+
         exit(1);
     }
 }
 
 // use first argument as output file or use "graph-composer-{version}.phar"
-$out = isset($argv[1]) ? $argv[1] : ('graph-composer-' . $version . '.phar');
+$output = isset($argv[1])
+    ? $argv[1]
+    : ('graph-composer-' . $version . '.phar');
 
-passthru('
-rm -rf build && mkdir build &&
-cp -r bin/ src/ composer.json LICENSE build/ &&
-sed -i \'s/@dev/' . $version .'/g\' build/src/App.php &&
-composer config -d build/ platform.php 5.3.6 &&
-composer install -d build/ --no-dev &&
-
-cd build/ && rm -rf bin/build.php vendor/*/*/tests/ vendor/*/*/examples/ vendor/*/*/*.md vendor/*/*/composer.* vendor/*/*/phpunit.* vendor/*/*/.gitignore vendor/*/*/.travis.yml && cd .. &&
-cd build/vendor/symfony/console/Symfony/Component/Console/ && rm -rf Tests/ *.md composer.* phpunit.* .gitignore && cd - &&
-vendor/bin/phar-composer build build/ ' . escapeshellarg($out) . ' &&
-
-php ' . escapeshellarg($out) . ' --version', $code);
+\passthru(\implode(' && ', [
+    'mkdir build/ || true',
+    \sprintf('php %s/vendor/bin/box compile', \dirname(__DIR__)),
+    \sprintf('mv build/graph-composer.phar build/%s', \escapeshellarg($output)),
+    \sprintf('php build/%s --version', \escapeshellarg($output)),
+]), $code);
 exit($code);
--- src/App.php
+++ src/App.php
@@ -13,4 +13,16 @@ class App extends BaseApplication
         $this->add(new Command\Show());
         $this->add(new Command\Export());
     }
+
+    public function getVersion(): string
+    {
+        // Since PHP-Scoper relies on COMPOSER_ROOT_VERSION the version parsed by PackageVersions, we rely on Box
+        // placeholders in order to get the right version for the PHAR.
+        if (\str_starts_with(__FILE__, 'phar:')) {
+            return '@git_version_placeholder@';
+        }
+
+        // Fallback to default if not running as PHAR.
+        return parent::getVersion();
+    }
 }
